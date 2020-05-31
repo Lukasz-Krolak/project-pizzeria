58822b5a944ff9c6ab21c59b16c4aab97925b37b
Łukasz Królak <lukasz.krolak@gmail.com>
2020-05-31 17:07:33 +0200

    8 thisApp

--- src/js/script.js
+++ src/js/script.js
@@ -62,8 +62,16 @@
 
   const app = {
     initMenu: function(){
+      const thisApp = this;
+
+      console.log('thisApp.data:',thisApp.data);
       const testProduct = new Product();
       console.log('testProduct:', testProduct);
+    },
+    initData: function(){
+      const thisApp = this;
+
+      thisApp.data = dataSource;
     },
     init: function(){
       const thisApp = this;
@@ -73,6 +81,7 @@
       console.log('settings:', settings);
       console.log('templates:', templates);
 
+      thisApp.initData();
       thisApp.initMenu();
     },
   };
