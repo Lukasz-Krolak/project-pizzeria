35ab75be776f38853dd8d5dcdfe603b6bd0d067b
Łukasz Królak <lukasz.krolak@gmail.com>
2020-06-06 16:50:59 +0200

    8 problem {

--- src/js/script.js
+++ src/js/script.js
@@ -84,47 +84,45 @@
       menuContainer.appendChild(thisProduct.element);
 
     }
-    initAccordion(){
-      const thisProduct = this;
-  
-      /* find the clickable trigger (the element that should react to clicking) */
-      thisProduct.element = document.querySelector(menuProducts);
-  
-      /* START: click event listener to trigger */
-      for (const element of elements){
-        element.addEventList('click',function(event){});{
-  
-          /* prevent default action for event */
-        
-          event.preventDefault();
-        
-        },
-        /* toggle active class on element of thisProduct */
-        thisProduct.element = classList.toggle(select.menuProducts,clickable);
-        /* find all active products */
-        const activeElements = document.querySelectorAll('product.active');
-
-        /* START LOOP: for each active product */
-        for (const activeElement of activeElements){
-  
-          /* START: if the active product isn't the element of thisProduct */
-          if (activeElement != thisProduct.element){
-  
-            /* remove class active for the active product */
-            activeElement.class.remove(classNames.menuProduct.wrapperActive);
-  
-          /* END: if the active product isn't the element of thisProduct */
-          }
-  
-          /* END LOOP: for each active product */ 
-        }
-  
-      /* END: click event listener to trigger */
-      }
-
-    } 
 
   }  
+  initAccordion(){
+    const thisProduct = this;
+
+    /* find the clickable trigger (the element that should react to clicking) */
+    thisProduct.element = document.querySelector(menuProduct);
+
+    /* START: click event listener to trigger */
+    for (const element of elements){
+
+      /* prevent default action for event */
+      element.addEventList('click',function(event){;
+        event.preventDefault();
+      },
+      /* toggle active class on element of thisProduct */
+        thisProduct.element = classList.toggle(classNames.menuProducts.wrapperActive));
+      /* find all active products */
+        const activeElements = document.querySelectorAll('product.active');
+
+      /* START LOOP: for each active product */
+      for (const activeElement of activeElements){;
+
+        /* START: if the active product isn't the element of thisProduct */
+        if (actuiveElement != thisProduct.element) {
+
+          /* remove class active for the active product */
+          activeElement.class.remove(classNames.menuProduct.wrapperActive);
+
+        /* END: if the active product isn't the element of thisProduct */
+        }
+
+        /* END LOOP: for each active product */ 
+      }
+
+    /* END: click event listener to trigger */
+    }
+
+  } 
   
 
   const app = {
