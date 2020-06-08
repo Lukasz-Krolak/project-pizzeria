5fd51cbf7ccf0958bef490bfaf2f1f2aeba55bfc
Łukasz Królak <lukasz.krolak@gmail.com>
2020-06-08 11:08:43 +0200

    8.6 And this is John Cena

--- src/js/script.js
+++ src/js/script.js
@@ -60,7 +60,10 @@
       thisProduct.data = data;
 
       thisProduct.renderInMenu();
+      thisProduct.getElements();
       thisProduct.initAccordion();
+      thisProduct.initOrderForm(); 
+      thisProduct.processOrder();
       console.log('new Product', thisProduct);
     }
     renderInMenu(){
@@ -83,14 +86,25 @@
       menuContainer.appendChild(thisProduct.element);
 
     }
+    getElements(){
+      const thisProduct = this;
+    
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+    }
     initAccordion(){
       const thisProduct = this;
 
       /* find the clickable trigger (the element that should react to clicking) */
-      const trigger = thisProduct.element.querySelector(select.menuProduct.clickable);
-
+      // const accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
       /* START: click event listener to trigger */
-      trigger.addEventListener('click',function(event){
+      
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+
+      thisProduct.accordionTrigger.addEventListener('click',function(event){
 
         /* prevent default action for event */
         
@@ -118,8 +132,70 @@
       /* END: click event listener to trigger */
       });
     
-    }  
+    } 
+    initOrderForm(){
+      const thisProduct = this;
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
       
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+      
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+      // console.log(initOrderForm);
+    }
+    processOrder(){
+      const thisProduct = this;
+    
+      /* read all data from the form (using utils.serializeFormToObject) and save it to const formData */
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      console.log('formData',formData);
+      /* set variable price to equal thisProduct.data.price */
+      let price = thisProduct.data.price; //let bo bedzie sie zmieniac 
+      /* START LOOP: for each paramId in thisProduct.data.params */
+      for (let paramId in thisProduct.data.params) {
+        /* save the element in thisProduct.data.params with key paramId as const param */
+        const param = thisProduct.data.params[paramId];
+        /* START LOOP: for each optionId in param.options */
+        for (let optionID in param.options){
+          /* save the element in param.options with key optionId as const option */
+          const option = param.options[optionID];
+          /* START IF: if option is selected and option is not default */
+
+          // rozwiązanie problemu co jesli dodatek  jest/ nie jest zaznaczony od razu (...)
+          const optionSelected = formData.hasOwnProperty(paramId) && formData[paramId].indexOf(optionID) > -1;
+          if(optionSelected && !option.default) { 
+            /* add price of option to variable price */
+            price += option.price; //dodaje cene dodatku
+            console.log('cena option.price', option.price);
+          /* END IF: if option is selected and option is not default */
+          /* START ELSE IF: if option is not selected and option is default */
+          } else if (!optionSelected && option.default){
+
+            /* deduct price of option from price */
+            price -= option.price;
+            console.log('John Cena', price);
+          }
+          /* END ELSE IF: if option is not selected and option is default */
+        }
+        /* END LOOP: for each optionId in param.options */
+      }
+      /* END LOOP: for each paramId in thisProduct.data.params */
+      thisProduct.priceSingle = price; 
+      thisProduct.priceElem.innerHTML = thisProduct.price;
+      console.log(thisProduct.params);
+    
+      /* set the contents of thisProduct.priceElem to be the value of variable price */
+    }
+                
   }
   const app = {
     initMenu: function(){
