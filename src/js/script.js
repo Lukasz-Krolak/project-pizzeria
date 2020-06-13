934c8f00d8e82400d708e3e65eb75f9bbc3ed7ed
Łukasz Królak <lukasz.krolak@gmail.com>
2020-06-13 14:18:25 +0200

    9.5 remove problem

--- src/js/script.js
+++ src/js/script.js
@@ -254,17 +254,6 @@
         /* END LOOP: for each optionId in param.options */
       }
       /* END LOOP: for each paramId in thisProduct.data.params */
-
-    
-      // /* set the contents of thisProduct.priceElem to be the value of variable price */
-      // price *= thisProduct.amountWidget.value;
-      // thisProduct.price = price;
-      // thisProduct.priceSingle = price;
-      // // console.log(price); 
-      // thisProduct.priceElem.innerHTML = thisProduct.price;
-      // console.log(thisProduct.priceElem);
-      
-
       //9.3
       /* multiply price by amount */
       thisProduct.priceSingle = price;
@@ -352,7 +341,7 @@
 
     announce() {
       const thisWidget = this;
-
+      //wlączenie bąbelkowania
       const event = new CustomEvent('updated', {
         bubbles: true
       });
@@ -368,6 +357,7 @@
       thisCart.deliveryFee = settings.cart.defaultDeliveryFee;
       thisCart.getElements(element);
       thisCart.initActions();
+
       console.log('new Cart', thisCart);
     }
     getElements(element) {
@@ -375,9 +365,14 @@
       thisCart.dom = {};
       thisCart.dom.wrapper = element;
       thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
-      thisCart.dom.productList = thisCart.dom.wrapper.querySelector(select.cart.productList);
-      
-    
+      thisCart.dom.productList = thisCart.dom.wrapper.querySelector(select.cart.productList);   
+
+      //9.4 wyswietlanie aktualnych sum
+      thisCart.renderTotalsKeys = ['totalNumber', 'totalPrice', 'subtotalPrice', 'deliveryFee'];
+
+      for(let key of thisCart.renderTotalsKeys){
+        thisCart.dom[key] = thisCart.dom.wrapper.querySelectorAll(select.cart[key]);
+      }
     }
     initActions(){
       const thisCart = this;
@@ -385,16 +380,16 @@
         event.preventDefault();
         thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
       });
-      thisCart.dom.productList.addEventListener('updated', function () {
-        thisCart.update();
+      thisCart.dom.productList.addEventListener('updated', function() {
+        thisCart.update(); // aktualizuje ilosc produktów w cart, czemu ilosc pod plus i minus dalej sie nie zmienia
       });
       thisCart.dom.productList.addEventListener('remove', function () {
         thisCart.remove(event.detail.cartProduct);
       });
-      thisCart.dom.productList.addEventListener('updated', function(){
-        thisCart.update();
-      });
+      
+
     }
+
     //9.3 powienie się produktu w koszyku, z poprawnymi informacjami
     add(menuProduct){
       const thisCart = this;
@@ -402,8 +397,117 @@
       const generatedDOM = utils.createDOMFromHTML(generatedHTML);
       console.log('GENERATEDDOM', generatedDOM);
       console.log('THISCARTPRODUCTLIST', thisCart.dom.productList);
-      const productList = thisCart.dom.productList;
-      productList.appendChild(generatedDOM);
+      thisCart.dom.productList.appendChild(generatedDOM);
+      thisCart.products.push(new CartProduct(menuProduct, generatedDOM));
+      console.log('thisCart.products', thisCart.products);
+      thisCart.update();//9.4
+      thisCart.remove();//9.5
+    }
+
+    //9.4 wyswietlanie aktualnych sum-- błąd update, błąd nie ta klasa
+    update() {
+      const thisCart = this;
+      thisCart.totalNumber = 0;
+      thisCart.subtotalPrice = 0;
+      console.log('thisCart.deliveryFee!!!', thisCart.deliveryFee);
+
+      for (let product of thisCart.products) {
+        thisCart.subtotalPrice += product.price;
+        thisCart.totalNumber += product.amount;
+      }
+
+      if(thisCart.subtotalPrice > 0) {
+        thisCart.totalPrice = thisCart.subtotalPrice + thisCart.deliveryFee;
+      } else {
+        thisCart.totalPrice = 0;
+      }
+      // 9.4 uaktualnienie renderTotalKeys
+      for(let key of thisCart.renderTotalsKeys){
+        for(let elem of thisCart.dom[key]) {
+          elem.innerHTML = thisCart[key];
+        }
+      }
+
+      console.log('totalNumber', thisCart.totalNumber);
+      console.log('subtotalPrice', thisCart.subtotalPrice);
+      console.log('thisCart.totalPrice', thisCart.totalPrice);
+    }
+    //9.5 remove is not a function? błąd nie może przeczytać "dom"
+    remove(cartProduct) {
+      const thisCart = this;
+      console.log(thisCart.products);
+      const index = thisCart.products.indexOf(cartProduct);
+      thisCart.products.splice(index, 1);
+      cartProduct.dom.wrapper.remove();
+      this.update();
+    }
+  }
+  //9.4 klasa pozwalajaca na funkcjonowanie pojedynczych pozycji w koszyku
+  class CartProduct {
+    constructor(menuProduct, element) {
+      const thisCartProduct = this;
+      
+      thisCartProduct.id = menuProduct.id;
+      thisCartProduct.name = menuProduct.name;
+      thisCartProduct.price = menuProduct.price;
+      thisCartProduct.priceSingle = menuProduct.priceSingle;
+      thisCartProduct.amount = menuProduct.amount;
+      thisCartProduct.params = JSON.parse(JSON.stringify(menuProduct.params));
+      
+      thisCartProduct.getElements(element);
+      thisCartProduct.initAmountWidget();
+      thisCartProduct.initActions();
+
+      console.log('new CartProduct', thisCartProduct);
+      console.log('productData', menuProduct);
+    }
+    getElements(element) {
+      const thisCartProduct = this;
+
+      thisCartProduct.dom = {};
+      thisCartProduct.dom.wrapper = element;
+      thisCartProduct.dom.amountWidget = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.amountWidget);
+      thisCartProduct.dom.price = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.price);
+      thisCartProduct.dom.edit = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.edit);
+      thisCartProduct.dom.remove = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.remove);
+    }
+    initAmountWidget(){
+      const thisCartProduct = this;
+      thisCartProduct.amountWidget = new AmountWidget(thisCartProduct.dom.amountWidget);
+
+      //9.4 tworzymy cene
+      thisCartProduct.dom.amountWidget.addEventListener('updated', function(){
+        console.log(thisCartProduct);
+        thisCartProduct.amount = thisCartProduct.amountWidget.value;
+        thisCartProduct.price = thisCartProduct.priceSingle * thisCartProduct.amount;
+        thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
+      });
+    }
+    //9.5 usuwamy produkt z koszyka
+    remove(){
+      const thisCartProduct = this;
+
+      const event = new CustomEvent('remove', {
+        bubbles: true,
+        detail: {
+          cartProduct: thisCartProduct,
+        },
+      });
+      thisCartProduct.dom.wrapper.dispatchEvent(event);
+      console.log(event);
+    }
+    //9.5 listenery
+    initActions(){
+      const thisCartProduct = this;
+
+      thisCartProduct.dom.edit.addEventListener('click', function (event){
+        event.preventDefault();
+      });
+      thisCartProduct.dom.remove.addEventListener('click', function (event){
+        event.preventDefault();
+        thisCartProduct.remove();
+
+      });
     }
 
   }
